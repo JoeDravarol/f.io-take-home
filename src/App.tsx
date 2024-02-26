85673e9b268fd2ba60c1921cf0541b592338a2c2
Joe Dravarol <joedravarol@gmail.com>
2024-02-26 18:20:43 +0000

    refactor: isolate data/state from view (JSX)
    Update useState hooks to store raw data instead of JSX element. It is
    not recommended to store anything other than data in useState.

--- src/App.tsx
+++ src/App.tsx
@@ -9,36 +9,47 @@ type CounterRef = {
   [label: string]: number;
 };
 
+type Counter = {
+  id: string;
+  label: string;
+};
+
+type Snapshot = {
+  id: string;
+  label: string;
+  value: number;
+};
+
 function App() {
-  const [counters, setCounters] = React.useState<JSX.Element[]>([]);
-  const [snapshots, setSnapshots] = React.useState<JSX.Element[]>([]);
+  const [counters, setCounters] = React.useState<Counter[]>([]);
+  const [snapshots, setSnapshots] = React.useState<Snapshot[]>([]);
 
   // Cache counter's data
   const counterRef = React.useRef<CounterRef | {}>({});
 
   const addNewCounter = () => {
-    const counterLabel = `Counter ${counters.length + 1}`;
+    const newCounter = {
+      id: crypto.randomUUID(),
+      label: `Counter ${counters.length + 1}`,
+    };
 
-    setCounters((prevState) => [
-      ...prevState,
-      <Counter key={counterLabel} label={counterLabel} ref={counterRef} />,
-    ]);
+    setCounters((prevState) => [...prevState, newCounter]);
   };
 
   const takeSnapshot = () => {
-    const counterSnapshots = Object.entries(counterRef.current).map(
-      ([label, value]) => (
-        <CounterSnapshot key={label} label={label} value={value} />
-      )
+    const countersData = Object.entries(counterRef.current).map(
+      ([label, value]) => ({ id: crypto.randomUUID(), label, value })
     );
 
-    setSnapshots(counterSnapshots);
+    setSnapshots(countersData);
   };
 
   return (
     <div className="wrapper">
       <section className="section section--wrapper">
-        {counters}
+        {counters.map((counter) => (
+          <Counter key={counter.id} label={counter.label} ref={counterRef} />
+        ))}
         <Button className="button--lg" onClick={addNewCounter}>
           Add
         </Button>
@@ -48,7 +59,13 @@ function App() {
         <Button className="button--lg" onClick={takeSnapshot}>
           Submit
         </Button>
-        {snapshots}
+        {snapshots.map((snapshot) => (
+          <CounterSnapshot
+            key={snapshot.id}
+            label={snapshot.label}
+            value={snapshot.value}
+          />
+        ))}
       </section>
     </div>
   );
