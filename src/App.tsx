89ae5d70fd17c713c9b2983df40f3c19f3358e2a
Joe Dravarol <joedravarol@gmail.com>
2024-02-26 15:12:08 +0000

    feat: dynamically add counters

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,28 @@ import React from 'react';
 
 import './App.css';
 import Counter from './components/Counter';
+import Button from './components/Button';
 
 function App() {
+  const [counters, setCounters] = React.useState<JSX.Element[]>([]);
+
+  const addNewCounter = () => {
+    const counterLabel = `Counter ${counters.length + 1}`;
+
+    setCounters((prevState) => [
+      ...prevState,
+      <Counter label={counterLabel} />,
+    ]);
+  };
+
   return (
-    <div>
-      <Counter label="Counter 1" />
+    <div className="wrapper">
+      <section className="section section--wrapper">
+        {counters}
+        <Button className="button--lg" onClick={addNewCounter}>
+          Add
+        </Button>
+      </section>
     </div>
   );
 }
